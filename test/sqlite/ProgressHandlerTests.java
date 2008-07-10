78edeb78ac03639146b8e6af8711f3d791ab9e4c
Igor Sereda <sereda@almworks.com>
2008-07-10 15:33:21 +0000

    upgrade sqlite to 3.5.9

--- test/sqlite/ProgressHandlerTests.java
+++ test/sqlite/ProgressHandlerTests.java
@@ -3,14 +3,8 @@ package sqlite;
 public class ProgressHandlerTests extends SQLiteConnectionFixture {
 
   public void testCancel() throws SQLiteException {
-    SQLiteConnection db = memDb().open(true);
-    db.exec("create table t (t integer)");
-    db.exec("begin");
-    for (int i = 0; i < 10; i++)
-      db.exec("insert into t values (" + i + ")");
-    db.exec("commit");
-
-    final SQLiteStatement st = db.prepare("select (a.t - b.t) * (c.t - d.t) * (e.t - f.t) from t a,t b,t c,t d,t e,t f order by 1");
+    SQLiteConnection db = prepareDb();
+    final SQLiteStatement st = longSelect(db);
     st.cancel();
     long start = System.currentTimeMillis();
     long time;
@@ -25,17 +19,8 @@ public class ProgressHandlerTests extends SQLiteConnectionFixture {
     st.reset();
 
 
-    new Thread() {
-      public void run() {
-        try {
-          Thread.sleep(1000);
-          st.cancel();
-        } catch (InterruptedException e) {
-          // ignore
-        }
-      }
-    }.start();
-    
+    interruptLater(st, 1000);
+
     start = System.currentTimeMillis();
     try {
       st.step();
@@ -47,5 +32,53 @@ public class ProgressHandlerTests extends SQLiteConnectionFixture {
     }
 
     db.dispose();
+  }
+
+  private void interruptLater(final SQLiteStatement st, final long wait) {
+    new Thread() {
+      public void run() {
+        try {
+          Thread.sleep(wait);
+          st.cancel();
+        } catch (InterruptedException e) {
+          // ignore
+        }
+      }
+    }.start();
+  }
+
+  private SQLiteStatement longSelect(SQLiteConnection db) throws SQLiteException {
+    return db.prepare("select (a.t - b.t) * (c.t - d.t) * (e.t - f.t) from t a,t b,t c,t d,t e,t f order by 1");
+  }
+
+  private SQLiteConnection prepareDb() throws SQLiteException {
+    SQLiteConnection db = memDb().open(true);
+    db.exec("create table t (t integer)");
+    db.exec("begin");
+    for (int i = 0; i < 10; i++)
+      db.exec("insert into t values (" + i + ")");
+    db.exec("commit");
+    return db;
+  }
+
+  public void testCancelAndTransactions() throws SQLiteException {
+    SQLiteConnection db = prepareDb();
+    SQLiteStatement st = longSelect(db);
+    db.exec("begin");
+    db.exec("insert into t values (1000)");
+    interruptLater(st, 1000);
+    try {
+      st.step();
+    } catch (SQLiteCancelledException e) {
+      // normal
+    }
+    SQLiteStatement chk = db.prepare("select 1 from t where t = 1000");
+    assertTrue(chk.step());
+    st.dispose();
+    chk.dispose();
+    db.exec("commit");
+    chk = db.prepare("select 1 from t where t = 1000");
+    assertTrue(chk.step());
+    chk.dispose();
   }
 }
