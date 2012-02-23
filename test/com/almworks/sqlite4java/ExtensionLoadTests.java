b740cf4abece41144106952b873ea84208dc1060
Igor Sereda <sereda@almworks.com>
2012-02-23 19:05:34 +0000

    fix extension test

--- test/com/almworks/sqlite4java/ExtensionLoadTests.java
+++ test/com/almworks/sqlite4java/ExtensionLoadTests.java
@@ -10,6 +10,23 @@ public class ExtensionLoadTests extends SQLiteConnectionFixture {
   @Override
   protected void setUp() throws Exception {
     super.setUp();
+    
+    String ext;
+    String osName = System.getProperty("os.name").toLowerCase();
+    if (osName.contains("windows")) {
+      ext = "32".equals(System.getProperty("sun.arch.data.model")) ? "x86" : "x64";
+    } else if (osName.contains("mac")) {
+      if (System.getProperty("os.version").startsWith("10.4")) {
+        ext = "osx-10.4";
+      } else if (System.getProperty("os.arch").toLowerCase().contains("power")) {
+        ext = "osx-ppc";
+      } else {
+        ext = "osx";
+      }
+    } else {
+      ext = "32".equals(System.getProperty("sun.arch.data.model")) ? "linux-i386" : "linux-amd64";
+    }
+
     URL url = getClass().getClassLoader().getResource(getClass().getName().replace('.', '/') + ".class");
     String path = url.getPath();
     int k = path.indexOf('!');
@@ -19,13 +36,13 @@ public class ExtensionLoadTests extends SQLiteConnectionFixture {
     while (dir != null) {
       File f = new File(new File(dir, "build"), "extension_sample");
       if (f.isDirectory()) {
-        myExtensionFile = new File(f, "half.sqlext." + System.getProperty("os.arch"));
+        myExtensionFile = new File(f, "half.sqlext." + ext);
         break;
       }
       dir = dir.getParentFile();
     }
     if (myExtensionFile == null) throw new IllegalStateException("cannot locate build directory");
-    if (!myExtensionFile.isFile()) throw new IllegalStateException("cannot find extension");
+    if (!myExtensionFile.isFile()) throw new IllegalStateException("cannot find extension " + myExtensionFile);
   }
 
   public void testLoadFailWhenNotEnabled() throws SQLiteException {
